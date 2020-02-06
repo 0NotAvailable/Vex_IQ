78d263e9d697546350ff3d94e217a8674a7378fe
rewinslett <rewinslett@gmail.com>
2020-02-06 16:50:51 -0600

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -8,20 +8,19 @@ turnRight(.68 ,rotations,60);
 }
 
 //* goes forward a number of rotation and speed
-void goforward(int rotation,int speed){
+void goforward(float rotation,int speed){
 setMotorSpeed(left, speed);
 setMotorSpeed(right, speed);
 int rotationsPerSecond = speed / 60;
-int time = rotation / rotationsPerSecond;
+float time = rotation / rotationsPerSecond;
 sleep(time * 1000);
 }
 
 //* takes cm and turns it to rotations
-int ToRotation(int num, int speed){
-int number = num + 3;
+float ToRotation(int num, int speed){
 int wheel_length = 6;
-int wheel_cer = (wheel_length * 3.14);
-int rotation = (number / wheel_cer);
+float wheel_cer = (wheel_length * 3.14);
+float rotation = (number / wheel_cer);
 return rotation;
 }
 
