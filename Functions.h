e57b99725a3e4cdd272eea703f995feb3baafdba
rewinslett <rewinslett@gmail.com>
2020-02-06 15:44:22 -0600

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -1,1 +1,37 @@
+void goL90(){
+turnLeft(.68 ,rotations,60);
+}
+
+//* goes Right 90 degrees
+void goR90(){
+turnRight(.68 ,rotations,60);
+}
+
+//* goes forward a number of rotation and speed
+void goforward(int rotation,int speed){
+setMotorSpeed(left, speed);
+setMotorSpeed(right, speed);
+int rotationsPerSecond = speed / 60;
+int time = rotation / rotationsPerSecond;
+sleep(time * 1000);
+}
+
+//* takes cm and turns it to rotations
+int ToRotation(int num, int speed){
+int number = num + 3;
+int wheel_length = 6;
+int wheel_cer = (wheel_length * 3.14);
+int rotation = (number / wheel_cer);
+return rotation;
+}
+
+void clawO(){
+setMotorSpeed(claws, 60);
+delay(200);
+}
+
+void clawC(){
+setMotorSpeed(claws, -60);
+delay(200);
+}
 
